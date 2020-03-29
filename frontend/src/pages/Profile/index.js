3ede65d6907b815da898200590841a181782f894
Ramon Rangel Lima <ramon.lima@radixeng.com.br>
2020-03-29 16:30:07 -0300

    Implementando logout e cadastro de casos

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg';
@@ -8,6 +8,8 @@ import "./styles.css"
 
 export default function Profile() {
     const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
 
     const ongId = localStorage.getItem('ongId'); 
     const ongName = localStorage.getItem('ongName');
@@ -36,6 +38,11 @@ export default function Profile() {
         }
     }
 
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -43,7 +50,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to={"/incidents/new"}>Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#e02041" />
                 </button>
             </header>
