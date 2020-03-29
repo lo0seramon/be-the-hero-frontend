8687f774f5f6230c90f07207bfa1dcdd394acf49
Ramon Rangel Lima <ramon.lima@radixeng.com.br>
2020-03-29 09:48:22 -0300

    Criando pagina de listagem de casos

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -15,7 +15,7 @@ export default function Register() {
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
-                    <Link className="back-link" to="/register">
+                    <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
                         Não tenho cadastro
                     </Link>
@@ -31,7 +31,7 @@ export default function Register() {
                         <input placeholder="UF" style={{ width: 80 }}/>
                     </div>
 
-                <button className="button" type="submit"></button>
+                <button className="button" type="submit">Cadastrar</button>
                 </form>
             </div>
         </div>
